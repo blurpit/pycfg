3515ef2de989021e835064e5f5049c3c6fd11520
blurpit <davidschneider821@gmail.com>
2021-03-16 16:35:08 -0700

    initial commit

---
+++ pycfg/__init__.py
@@ -0,0 +1,325 @@
+import codecs
+from abc import abstractmethod
+from configparser import ConfigParser, NoSectionError, NoOptionError, DuplicateSectionError, DuplicateOptionError
+
+
+class ConfigFile:
+    """ Base class for config objects """
+    __immutable__ = False
+    __autowrite__ = False
+
+    def __init__(self, filename=None, **kwargs):
+        self.filename = filename
+
+        # Overwrite class options
+        self.__immutable__ = kwargs.get('immutable', self.__immutable__)
+        self.__autowrite__ = kwargs.get('autowrite', self.__autowrite__)
+
+        self.parser:ConfigParser = None
+        self._sections = {}
+        if self.filename:
+            self.read()
+
+    def read(self, filename:str=None, encoding=None):
+        """
+        Read config file.
+
+        :param filename: Filepath to read, if not already given in the constructor.
+        :param encoding: File encoding. Default is 'utf8'
+        """
+        if filename:
+            self.filename = filename
+        if not self.filename:
+            raise FileNotFoundError("No file was given.")
+
+        self.parser = ConfigParser()
+        self.parser.optionxform = str
+
+        self.parser.read_file(codecs.open(self.filename, encoding=encoding))
+        self.create()
+        for section in self:
+            section._parse(self.parser)
+
+    def register(self, section):
+        if section in self:
+            raise DuplicateSectionError(section)
+        section.cfg = self
+        self._sections[section.name] = section
+
+    def create_section(self, name, *options):
+        self.parser.add_section(name)
+        Section(self, name, *options)
+
+    def remove_section(self, section):
+        if section not in self:
+            raise NoSectionError(section)
+        self.parser.remove_section(section)
+        self._sections.pop(section)
+
+    def __getitem__(self, section:str):
+        if section not in self:
+            raise NoSectionError(section)
+        return self._sections[section]
+
+    def set(self, section, option, value):
+        if self.__immutable__:
+            raise PermissionError("Config file '%s' is immutable." % self.filename)
+
+        if not isinstance(section, Section):
+            section = self[section]
+        if not isinstance(option, Option):
+            option = section.ref(option)
+
+        if option.typecheck is not None and not option.typecheck(value):
+            raise TypeError("Wrong type for option %s: %s, expected: %s." % (option.name, value, option.dtype))
+        option.set(value)
+
+        if not isinstance(option, UnlinkedOption):
+            self.parser.set(
+                section.name, option.name,
+                option.to_str(value)
+            )
+            if self.__autowrite__:
+                self.write()
+
+    def write(self):
+        """ Writes changes to the config parser to the file. """
+        if not self.__immutable__:
+            with open(self.filename, 'w', encoding='utf8') as file:
+                self.parser.write(file)
+
+    def __len__(self):
+        return len(self._sections)
+
+    def __iter__(self):
+        return iter(self._sections.values())
+
+    @property
+    def section_names(self):
+        return list(self._sections.keys())
+
+    def __contains__(self, section):
+        return section in self._sections
+
+    @abstractmethod
+    def create(self):
+        """ Override this method in a child class to parse config options """
+        raise NotImplementedError('PyCfgFile needs to implement the create() method.')
+
+    def __str__(self):
+        return "<%s at %s>" % (self.__class__.__name__, self.filename or '(unknown)')
+
+
+class Section:
+    def __init__(self, cfg:ConfigFile, name:str, *options):
+        if not isinstance(name, str):
+            raise TypeError("Section name must be a string.")
+        if len(name) == 0:
+            raise ValueError("Section name cannot be empty.")
+        self.cfg = cfg
+        self.name = name
+
+        self._options = {}
+        for option in options:
+            self.register(option)
+
+        if self.cfg is not None:
+            self.cfg.register(self)
+
+    def register(self, option):
+        if isinstance(option, OptionCollection):
+            option._register(self)
+            return
+
+        if option.name in self:
+            raise DuplicateOptionError(self.name, option.name)
+        option.sec = self
+        self._options[option.name] = option
+        if hasattr(option, '_initial_val'):
+            self.set(option, option._initial_val)
+            delattr(option, '_initial_val')
+
+    def ref(self, option):
+        if option not in self:
+            raise NoOptionError(option, self.name)
+        return self._options[option]
+
+    def get(self, option, fallback=None):
+        return self[option] if option in self else fallback
+
+    def set(self, option, value):
+        self.cfg.set(self, option, value)
+
+    def raw(self, option, fallback=None):
+        return self.ref(option).raw if option in self else fallback
+
+    def __getitem__(self, option):
+        return self.ref(option).get_value()
+
+    def __setitem__(self, option, value):
+        self.set(option, value)
+
+    def __len__(self):
+        return len(self._options)
+
+    def __iter__(self):
+        return iter(self._options)
+
+    def __contains__(self, name):
+        return name in self._options
+
+    def keys(self):
+        return self._options.keys()
+
+    def values(self):
+        for k in self:
+            yield self[k]
+
+    def items(self):
+        for k in self:
+            yield k, self[k]
+
+    def _parse(self, parser:ConfigParser):
+        for option in self._options.values():
+            option._parse(parser, self.name)
+
+    def __str__(self):
+        return "<Section '%s'>" % self.name
+
+    def __repr__(self):
+        return "Section('%s')" % self.name
+
+
+class Option:
+    REFERENCE = object()
+    __dtype__ = str
+
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.sec:Section = None
+
+        self.dtype = kwargs.get('dtype', self.__dtype__)
+        if 'value' in kwargs: self._initial_val = kwargs['value']
+
+    def set(self, value):
+        self.value = value
+
+    def from_str(self, string:str):
+        """  """
+        return self.dtype(string)
+
+    def to_str(self, value):
+        return str(value)
+
+    def typecheck(self, value):
+        return self.dtype is None or isinstance(value, self.dtype)
+
+    def _parse(self, parser:ConfigParser, section):
+        """ Read the option value from the config file """
+        self.raw = parser.get(section, self.name)
+        self.value = self.raw.replace('\n', ' ').strip()
+
+        if self.value.lower() in ('none', 'null', ''):
+            self.value = None
+        else:
+            self.value = self.from_str(self.value)
+
+    def get_value(self):
+        if self.value is Option.REFERENCE: return self
+        elif callable(self.value): return self.value()
+        else: return self.value
+
+    def __str__(self):
+        return "<%s '%s/%s'>" % (self.__class__.__name__, self.sec.name, self.name)
+
+    def __repr__(self):
+        return "%s('%s')" % (self.__class__.__name__, self.name)
+
+class UnlinkedOption(Option):
+    def _parse(self, parser, section):
+        # Unlinked, nothing to read
+        pass
+
+class DependentOption(UnlinkedOption):
+    def __init__(self, name, value=None, references=None, **kwargs):
+        super().__init__(name, **kwargs)
+        self._func = value or (lambda *args: None)
+        self._refs = references or []
+        if isinstance(self._refs, tuple):
+            self._refs = list(self._refs)
+        elif not isinstance(self._refs, list):
+            self._refs = [self._refs]
+        self._parse_references(self._refs)
+
+    def set(self, value):
+        raise PermissionError("Cannot set value on dependent option.")
+
+    def _parse_references(self, refs):
+        for i, reference in enumerate(refs):
+            if isinstance(reference, str):
+                reference = reference.split('/')
+            elif not isinstance(reference, (tuple, list)):
+                raise TypeError("Unexpected type for dependent option reference: %s %s, expected: str, tuple, list" % (reference, type(reference)))
+
+            if len(reference) == 1:
+                reference = (None, reference[0])
+            if len(reference) > 2 or len(reference) <= 0:
+                raise ValueError("Dependent option reference must be 1 or 2 elements in length (section, option), got: %s" % str(reference))
+            elif not (reference[0] is None or isinstance(reference[0], str) and isinstance(reference[1], str)):
+                raise ValueError("Dependent option references must be strings, got: %s" % str(reference))
+
+            refs[i] = tuple(reference)
+
+    def value(self):
+        return self._func(*(self.sec.cfg[ref[0] or self.sec.name][ref[1]] for ref in self._refs))
+
+class OptionCollection:
+    def __init__(self, option_cls, *args, **kwargs):
+        if not isinstance(option_cls, type):
+            raise TypeError("Class expected for OptionCollection type, got '%s'." % option_cls)
+        if not issubclass(option_cls, Option):
+            raise TypeError("OptionCollection type %s does not inherit from Option." % option_cls)
+        self._cls = option_cls
+        self._filter = kwargs.get('name_filter')
+        self._limit = kwargs.get('match_limit')
+        self._args = args
+        self._kwargs = kwargs
+
+    def _register(self, sec):
+        count = 0
+        for name in sec.cfg.parser[sec.name]:
+            if name not in sec \
+                    and (self._limit is None or count < self._limit) \
+                    and (not self._filter or self._filter(name)):
+                sec.register(self._cls(name, *self._args, **self._kwargs))
+                count += 1
+
+class SectionCollection:
+    class Option:
+        def __init__(self, option_cls, *args, **kwargs):
+            if not isinstance(option_cls, (type, OptionCollection)):
+                raise TypeError("Class or OptionCollection expected for SectionCollection.Option type, got %s." % option_cls)
+            if not (isinstance(option_cls, OptionCollection) or issubclass(option_cls, Option) or issubclass(option_cls, OptionCollection)):
+                raise TypeError("SectionCollection.Option type %s does not inherit from Option." % option_cls)
+            self._cls = option_cls
+            self._args = args
+            self._kwargs = kwargs
+
+        def __call__(self):
+            if isinstance(self._cls, OptionCollection): return self._cls
+            return self._cls(*self._args, **self._kwargs)
+
+    def __init__(self, cfg:ConfigFile, *options:Option, **kwargs):
+        self._filter = kwargs.get('name_filter')
+        self._limit = kwargs.get('match_limit')
+        self._register(cfg, options)
+
+    def _register(self, cfg, options):
+        count = 0
+        for name in cfg.parser:
+            if name != 'DEFAULT' \
+                    and name not in cfg \
+                    and (self._limit is None or count < self._limit) \
+                    and (not self._filter or self._filter(name)):
+                Section(cfg, name, *(option() for option in options))
+                count += 1
---
+++ pycfg/options.py
@@ -0,0 +1,167 @@
+import codecs
+import datetime as dt
+import decimal
+import json
+import pickle
+from typing import Sequence, Collection
+
+from . import Option
+
+
+class StrOption(Option):
+    pass
+
+class IntOption(Option):
+    """ Option with int datatype """
+    __dtype__ = int
+
+class FloatOption(Option):
+    """ Option with float datatype """
+    __dtype__ = float
+
+class DecimalOption(Option):
+    """ Option with decimal.Decimal datatype """
+    __dtype__ = decimal.Decimal
+
+class BoolOption(Option):
+    """ Option with bool datatype. Values passed into  """
+    __dtype__ = bool
+
+    def __init__(self, name, true_values=('true', 'yes', 'on', 'enabled'), **kwargs):
+        super().__init__(name, **kwargs)
+        self.true_values = true_values
+
+    def from_str(self, string):
+        return string.lower() in self.true_values
+
+class ListOption(Option):
+    def __init__(self, name, delimiter=', ', force_dtype=True, **kwargs):
+        super().__init__(name, **kwargs)
+        self.delimiter = delimiter
+        self._force_dtype = force_dtype
+
+    def set(self, value):
+        self.value = list(value)
+
+    def from_str(self, string):
+        if not string:
+            return []
+        result = string.split(self.delimiter)
+        if self.dtype != str:
+            result = list(map(self.dtype, result))
+        return result
+
+    def to_str(self, value):
+        return self.delimiter.join(map(str, value))
+
+    def typecheck(self, value):
+        if not isinstance(value, Collection):
+            raise TypeError("Wrong type for option %s: %s, expected typing.Collection" % (self.name, value))
+        elif self._force_dtype:
+            return all(isinstance(element, self.dtype) for element in value)
+        return True
+
+class TupleOption(ListOption):
+    def set(self, value):
+        self.value = tuple(value)
+
+    def from_str(self, string):
+        return tuple(super().from_str(string))
+
+class SetOption(ListOption):
+    def set(self, value):
+        self.value = set(value)
+
+    def from_str(self, string):
+        return set(super().from_str(string))
+
+class RangeOption(Option):
+    __dtype__ = tuple
+
+    def __init__(self, name, delimiter='-', **kwargs):
+        super().__init__(name, dtype=Sequence, reference=True, **kwargs)
+        self.delimiter = delimiter
+        self.start = 0
+        self.stop = 0
+
+    def set(self, value):
+        """ Set the range. Value must be a tuple of (start, stop) """
+        self.start, self.stop = value
+        self.value = Option.REFERENCE
+
+    def set_start(self, start):
+        self.set((start, self.stop))
+
+    def set_stop(self, stop):
+        self.set((self.start, stop))
+
+    def from_str(self, string):
+        start, stop = string.split(self.delimiter)
+        self.start = float(start.strip())
+        self.stop = float(stop.strip())
+        return Option
+
+    def to_str(self):
+        return str(self.start) + self.delimiter + str(self.stop)
+
+    @property
+    def size(self):
+        return self.stop - self.start
+
+    def __contains__(self, item):
+        return self.start <= item <= self.stop
+
+    def __str__(self):
+        return "RangeOption(%s, %s)" % (self.start, self.stop)
+
+class DateTimeOption(Option):
+    ISOFORMAT = object()
+    __dtype__ = dt.datetime
+
+    def __init__(self, name, fmt=ISOFORMAT, **kwargs):
+        super().__init__(name, **kwargs)
+        self.fmt = fmt
+
+    def from_str(self, string):
+        if self.fmt == self.ISOFORMAT:
+            return dt.datetime.fromisoformat(string)
+        else:
+            return dt.datetime.strptime(string, self.fmt)
+
+    def to_str(self, value:dt.datetime):
+        if self.fmt == self.ISOFORMAT:
+            return value.isoformat()
+        else:
+            return value.strftime(self.fmt)
+
+class DateOption(DateTimeOption):
+    ISOFORMAT = "%Y-%m-%d"
+    __dtype__ = dt.date
+
+    def from_str(self, string):
+        return super().from_str(string).date()
+
+class PickleOption(Option):
+    __dtype__ = None
+
+    def __init__(self, name, pickler=pickle, encoding='base64', **kwargs):
+        super().__init__(name, **kwargs)
+        self.pickler = pickler
+        self.encoding = encoding
+
+    def from_str(self, string:str):
+        return self.pickler.loads(codecs.decode(string.encode(), self.encoding))
+
+    def to_str(self, value):
+        return codecs.encode(self.pickler.dumps(value), self.encoding).decode()
+
+class JSONOption(Option):
+    __dtype__ = None
+
+    def from_str(self, string:str):
+        return json.loads(string)
+
+    def to_str(self, value):
+        return json.dumps(value)
+
+DictOption = JSONOption
