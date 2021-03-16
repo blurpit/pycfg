cb2da332a75ab2c784dc71ec58422a238e93f566
blurpit <davidschneider821@gmail.com>
2021-03-16 16:45:13 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="pycfg",
+    version="1.0",
+    author="David Schneider",
+    description="Custom package for working with cfg files easier",
+    url="https://github.com/blurpit/pycfg",
+    packages=["pycfg"],
+    python_requires=">=3.7",
+)
