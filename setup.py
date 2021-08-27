96724530a8bf222c69d46c0e4891d90963696f98
blurpit <davidschneider821@gmail.com>
2021-08-27 16:39:40 -0600

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pycfg",
-    version="1.0.3",
+    version="1.0.4",
     author="David Schneider",
     description="Custom package for working with cfg files easier",
     url="https://github.com/blurpit/pycfg",
