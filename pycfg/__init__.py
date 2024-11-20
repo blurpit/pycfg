6b25e3a70b3e5ddc008ed91c620ea662a3b42c06
blurpit <davidschneider821@gmail.com>
2024-11-20 14:37:08 -0700

    Fix SectionCollection not in __init__

--- pycfg/__init__.py
+++ pycfg/__init__.py
@@ -1,3 +1,3 @@
-from .cfg import ConfigFile, Option, Section, UnlinkedOption
+from .cfg import ConfigFile, Option, Section, UnlinkedOption, SectionCollection
 from .options import BoolOption, DateOption, DateTimeOption, DecimalOption, DerivedOption, DictOption, FloatOption, \
     IntOption, JsonOption, ListOption, OptionCollection, PickleOption, RangeOption, StrOption
