b76274c67eeb9cde70f9f5d2d049f37176f9877e
blurpit <davidschneider821@gmail.com>
2024-11-20 16:14:49 -0700

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pycfg",
-    version="1.1.1",
+    version="1.1.2",
     author="David Schneider",
     author_email="davidschneider821@gmail.com",
     description="A package for defining config file structure in python, "
